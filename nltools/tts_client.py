e0d0f516f9f35df0a40617fcdcd93a052a0e3418
Guenter Bartsch <guenter@zamia.org>
2017-02-01 23:35:16 +0100

    add tts client implementation

---
+++ nltools/tts_client.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#
+# Copyright 2016, 2017 Guenter Bartsch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# client utility functions to access our HTTP TTS server
+#
+
+import traceback
+import json
+from base64 import b64encode
+import logging
+import requests
+import urllib
+
+MARY_VOICES = {
+
+    'en_US': { 'male':   [ "cmu-rms-hsmm", "dfki-spike", "dfki-obadiah", "dfki-obadiah-hsmm", "cmu-bdl-hsmm"],
+               'female': [ "cmu-slt-hsmm", "dfki-poppy", "dfki-poppy-hsmm", "dfki-prudence", "dfki-prudence-hsmm" ]
+             },
+
+    'de_DE': { 'male':   ["bits3", "bits3-hsmm", "dfki-pavoque-neutral", "dfki-pavoque-neutral-hsmm", "dfki-pavoque-styles"],
+               'female': ["bits1-hsmm"]
+             }
+    }
+
+ESPEAK_VOICES = ['en', 'de']
+
+
+class TTSClient(object):
+
+    def __init__(self, host_tts='localhost', port_tts=8300, locale='en_US', engine='mary', voice='cmu-rms-hsmm'):
+
+        self.host_tts = host_tts
+        self.port_tts = port_tts
+        self.locale  = locale
+        self.engine = engine
+        self.voice  = voice
+
+    def set_locale(self, locale):
+        self.locale = locale
+
+    def set_voice(self, voice):
+        self.voice = voice
+
+    def set_engine(self, engine):
+        self.engine = engine
+
+    def synthesize(self, utterance, mode='txt'):
+
+        args = {'l': self.locale,
+                'v': self.voice,
+                'e': self.engine,
+                'm': mode,
+                't': utterance.encode('utf8')}
+        url = 'http://%s:%s/tts/synth?%s' % (self.host_tts, self.port_tts, urllib.urlencode(args))
+
+        response = requests.get(url)
+
+        assert response.status_code == 200
+
+        wav = response.content
+
+        return wav
+
+    def play_wav (self, wav):
+
+        url = 'http://%s:%s/tts/play' % (self.host_tts, self.port_tts)
+                          
+        response = requests.post(url, data=wav)
+
+    def say (self, utterance):
+
+        wav = self.synthesize(utterance)
+        self.play_wav(wav)
+
+    def say_ipa (self, ipa):
+
+        wav = self.synthesize(utterance, mode='ipa')
+        self.play_wav(wav)
+
+    def gen_ipa (self, word):
+
+        args = {'l': self.locale,
+                'v': self.voice,
+                'e': self.engine,
+                't': word.encode('utf8')}
+        url = 'http://%s:%s/tts/g2p?%s' % (self.host_tts, self.port_tts, urllib.urlencode(args))
+
+        response = requests.get(url)
+
+        assert response.status_code == 200
+
+        return response.json()['ipa']
+
+
---
+++ tests/test_tts_client.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#
+# Copyright 2014, 2016, 2017 Guenter Bartsch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# these tests read tts settings from ~/.speechrc
+#
+
+import unittest
+import logging
+
+from nltools.tts_client import TTSClient
+from nltools import misc
+
+MARY_TESTS = [
+              ('de_DE', 'bits3',            u'UNMUTE',           u"'\u0294\u028an-'mu\u02d0-t\u0259"),
+              ('de_DE', 'bits3',            u"DÜSTRE",           u"'d\u028fs-t\u0281\u0259"),
+              ('de_DE', 'bits3',            u"EINGANGE",         u"'a\u026a-\u0273a-\u0273\u0259"),
+              ('en_US', 'cmu-rms-hsmm',     u'hello',            u"h\u0259-'l\u0259\u028a"),
+              ('fr_FR', 'upmc-pierre-hsmm', u'bonjour',          u"'bo-\u0292u\u0281"),
+             ]
+
+ESPEAK_TESTS = [
+                ('de', u'GELBSEIDENEN',     u"g'\u025blbza\u026ad\u0259n\u0259n"),
+                ('de', u'UNMUTE',           u"'\u028anmu\u02d0t\u0259"),
+               ]
+class TestTTSClient (unittest.TestCase):
+
+    def test_tts_client(self):
+
+        config = misc.load_config('.speechrc')
+        
+        tts = TTSClient(config.get('tts', 'host'), int(config.get('tts', 'port')))
+
+        # test mary
+
+        tts.set_engine('mary')
+
+        for l, voice, word, ph in MARY_TESTS:
+
+            tts.set_locale(l)
+            tts.set_voice(voice)
+
+            mary_ph = tts.gen_ipa (word)
+
+            self.assertEqual (mary_ph, ph)
+
+            wav = tts.synthesize (word)
+            logging.debug('wav len: %d bytes.' % len(wav))
+            self.assertGreater (len(wav), 100)
+
+            wav = tts.synthesize (ph, mode='ipa')
+            logging.debug('wav len: %d bytes.' % len(wav))
+            self.assertGreater (len(wav), 100)
+
+            # tts.say (word)
+            # tts.play_wav(wav)
+
+        # test espeak
+
+        tts.set_engine('espeak')
+
+        for v, word, ph in ESPEAK_TESTS:
+
+            tts.set_locale(v)
+            tts.set_voice(v)
+
+            espeak_ph = tts.gen_ipa (word)
+
+            self.assertEqual (espeak_ph, ph)
+
+            wav = tts.synthesize (word)
+            logging.debug('wav len: %d bytes.' % len(wav))
+            self.assertGreater (len(wav), 100)
+
+            wav = tts.synthesize (ph, mode='ipa')
+            logging.debug('wav len: %d bytes.' % len(wav))
+            self.assertGreater (len(wav), 100)
+
+            # tts.say (word)
+            # tts.play_wav(wav)
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.ERROR)
+    # logging.basicConfig(level=logging.DEBUG)
+
+    unittest.main()
+
