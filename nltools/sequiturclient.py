4ca5fcb40a27fe8c0249f10a583704f55a7a42f8
Guenter Bartsch <guenter@zamia.org>
2017-10-06 01:39:51 +0200

    multi mode

--- nltools/sequiturclient.py
+++ nltools/sequiturclient.py
@@ -65,3 +65,44 @@ def sequitur_gen_ipa(modelfn, word):
 
     return ipa
 
+def sequitur_gen_ipa_multi(modelfn, words):
+
+    ipa_map ={}
+
+    with tempfile.NamedTemporaryFile() as f:
+
+        for word in words:
+            f.write((u'%s\n' % word).encode('utf8'))
+        f.flush()
+
+        cmd = ['g2p.py', '--model', modelfn, '--apply', f.name]
+
+        res = misc.run_command(cmd)
+
+        logging.debug('%s' % ' '.join(cmd))
+
+        for l in res:
+
+            line = l.strip()
+
+            logging.debug('%s' % line)
+
+            if 'stack usage:' in line:
+                continue
+
+            parts = line.decode('utf8').split('\t')
+
+            if len(parts) < 2:
+                continue
+
+            word = parts[0]
+            if word in words:
+
+                xs = parts[1]
+                # print 'XS', xs
+           
+                ipa = xsampa2ipa(word, xs)
+                ipa_map[word] = ipa
+
+    return ipa_map
+
