dda85ecde5518622eb037ee864a52afe3d63d9b9
Guenter Bartsch <guenter@zamia.org>
2018-07-02 22:57:20 +0200

    0.2.3 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name                 = 'py-nltools',
-    version              = '0.2.2',
+    version              = '0.2.3',
     description          = 'A collection of basic python modules for spoken natural language processing',
     long_description     = open('README.md').read(),
     author               = 'Guenter Bartsch',
